ea5a774a467325a7b6fc56a44903c73ee88d6324
loansindi <derek.bever@gmail.com>
2014-04-23 21:08:14 -0500

    Add too many comments to sketch.

--- piezo_tone.ino
+++ piezo_tone.ino
@@ -1,19 +1,23 @@
 // Crappy piezo optical theremin. Depends on attiny85 arduino hardware files.
 
 void setup() {
-	pinMode(0, OUTPUT);
-	pinMode(1, OUTPUT);
-	pinMode(2, INPUT);
+	pinMode(0, OUTPUT); // We use pins 0 and 1 as differential outputs for the piezo buzzer
+	pinMode(1, OUTPUT); // this offers improved loudness compared to a single pin
+	pinMode(2, INPUT);  // pin 2 is our ADC input - reading the voltage divider formed by the photoresistor and regular resistor
 }
 
 
 unsigned int pitch = 40;
 
 void loop() {
-	int pitch = 5 * analogRead(1);
-	beep (0, 1, pitch, 50); // piezo is connected to two outputs that switch differentially for greater volume
-	//changing the fourth argument of this function changes the 'quantization' effect of the generated tone.
-
+	int pitch = 5 * analogRead(1); //the 10 bit ADC produces a number 0-1023, so our frequency range is 0-5115 in 5hz increments
+	beep (0, 1, pitch, 50); 
+	// This function takes four arguments, as copied from the internet
+  // the first argument is the pin to generate a tone on - this should be refactored out for this purpose
+  // the second argument is added by me - it's the second pin for differential buzzing
+  // the third argument is the target frequency, and the 4th determines the length of the tone
+  // changing the fourth argument of this function changes the 'quantization' effect of the generated tone.
+  // shorter lengths produce a glissando effect, longer lengths create a distinct series of steps
 
 }
   void beep (unsigned char speakerPin, unsigned char speakerPin2, int frequencyInHertz, long timeInMilliseconds)
